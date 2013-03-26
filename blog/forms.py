8f4cca7e618dfe6fcd32a50078573b2d723d2c27
Mateusz Kieblesz <mateuszkieblesz@gmail.com>
2013-03-26 13:53:43 +0000

    form error message

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from models import Article
 
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=200)
-    body = forms.CharField(label="", help_text="", widget=forms.Textarea)
+    body = forms.CharField(widget=forms.Textarea)
     is_published = forms.BooleanField(required=False)
     
     def is_valid(self, edit=False):
@@ -13,6 +13,8 @@ class ArticleForm(forms.Form):
         if not valid:
             return valid
         if not edit and db.Query(Article).filter("title =", self.cleaned_data['title']).get():
-            self._errors['title'] = 'Article with this title exist'
+        
+            self.errors['title'] = 'Article with this title exist'
+            print self.errors
             return False
         return True
